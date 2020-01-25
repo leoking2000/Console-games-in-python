012b1b8023e84ea99afe8382f2bd03dfbb5957d9
leoking <Leoking470@gmail.com>
2020-01-25 19:43:24 +0200

    _

--- snake/gamelogic.py
+++ snake/gamelogic.py
@@ -4,11 +4,30 @@ class Point:
         self.y = y
 
 class Cell(Point):
-    def __init__(self, x, y, speedX=0, speedY=0):
+
+    def __init__(self, x, y, speed):
         super().__init__(x,y)
-        self.speedX = speedX
-        self.speedY = speedY
+        self.speed = speed
+        self.speedX = self.speed
+        self.speedY = 0
 
     def update(self):
         self.x += self.speedX
         self.y += self.speedY
+
+    def go_down(self):
+        if self.speedY == 0:
+            self.speedY = self.speed
+            self.speedX = 0
+    def go_up(self):
+        if self.speedY == 0:
+            self.speedY = -self.speed
+            self.speedX = 0
+    def go_right(self):
+        if self.speedX == 0:
+            self.speedX = self.speed
+            self.speedY = 0
+    def go_left(self):
+        if self.speedX == 0:
+            self.speedX = -self.speed
+            self.speedY = 0
--- snake/test.py
+++ snake/test.py
@@ -4,44 +4,42 @@ import pygame
 
 green = (0,255,0)
 red = (255,0,0)
+black = (0, 0, 0)
+display_width = 600
+display_height = 600
+FPS = 10
+block_size = 10
 
 pygame.init()
-gameDisplay = pygame.display.set_mode((600,600))
+gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption("Snake-Game") #add icon?
-# game clock
-clock = pygame.time.Clock()
+clock = pygame.time.Clock() # game clock
 
 gameRunning = True
-speed = 10
-player = Cell(100, 100, speedX=speed)
-
+player = Cell(display_width // 2, display_height // 2, speed=block_size)
 
 while gameRunning:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            gameRunning = False
         if event.type == pygame.KEYDOWN :
             if event.key == pygame.K_UP or event.key == pygame.K_w :
-                player.speedY = -speed
-                player.speedX = 0
+                player.go_up()
             elif event.key == pygame.K_DOWN or event.key == pygame.K_s :
-                player.speedY = speed
-                player.speedX = 0
+                player.go_down()
             elif event.key == pygame.K_RIGHT or event.key == pygame.K_d :
-                player.speedX = speed
-                player.speedY = 0
+                player.go_right()
             elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                player.speedX = -speed
-                player.speedY = 0
+                player.go_left()
+        elif event.type == pygame.QUIT:
+            gameRunning = False
+
+    if  player.x >= display_width or player.x < 0 or player.y >= display_height or player.y < 0 :
+        gameRunning = False
 
     player.update()
-
-    gameDisplay.fill((0, 0, 0))
-
-    pygame.draw.rect(gameDisplay, red, [player.x, player.y, 10, 10])
-
+    gameDisplay.fill(black)
+    pygame.draw.rect(gameDisplay, red, [player.x, player.y, block_size, block_size])
     pygame.display.update()
-    clock.tick(10)
+    clock.tick(FPS)
 
 pygame.quit()
 quit()
